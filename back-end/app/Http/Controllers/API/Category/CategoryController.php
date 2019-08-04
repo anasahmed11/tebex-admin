de76b8ba6fe1b7768b7bb99695c5b381c8a3a010
Ahmed Bally <ahmed30816896@f-eng.tanta.edu.eg>
2019-08-04 23:30:09 +0200

    fixed

--- back-end/app/Http/Controllers/API/Category/CategoryController.php
+++ back-end/app/Http/Controllers/API/Category/CategoryController.php
@@ -3,8 +3,13 @@
 namespace App\Http\Controllers\API\Category;
 
 use App\Category;
+use App\Http\Resources\ProductCollection;
 use Illuminate\Http\Request;
 use App\Http\Controllers\Controller;
+use Illuminate\Pagination\LengthAwarePaginator;
+use Illuminate\Pagination\Paginator;
+use Illuminate\Support\Collection;
+use Illuminate\Support\Facades\Input;
 
 class CategoryController extends Controller
 {
@@ -16,9 +21,26 @@ class CategoryController extends Controller
         return response()->json($category->Specs()->get());
     }
     public function products(Category $category){
-        return response()->json($category->Product()->get());
+        $cats=$category::get()->toFlatTree();
+        $products=$cats[0]->Product()->get();
+        for ($i=1;$i < $cats->count();$i++){
+           $products=$products->merge($cats[$i]->Product()->get());
+        }
+        $pages=($this->paginate($products->all(),Input::get("perpage")??30,Input::get("page")??1));
+        $response=[];
+        $response['data']=$pages->all();
+        $response['paging']['current']=$pages->currentPage();
+        $response['paging']['last']=$pages->lastPage();
+        $response['paging']['items']=$pages->total();
+        return response()->json($response);
     }
     public function filter(Category $category){
         return response()->json($category->Product()->get());
+    }
+    public function paginate($items, $perPage = 30, $page = null, $options = [])
+    {
+        $page = $page ?: (Paginator::resolveCurrentPage() ?: 1);
+        $items = $items instanceof Collection ? $items : Collection::make($items);
+        return new LengthAwarePaginator($items->forPage($page, $perPage), $items->count(), $perPage, $page, $options);
     }
 }
