17d4238d4a96ce8341e2af12feb0d203387c50da
Abdelrahman Tarek <abdelrahman.t.abdelhamid@gmail.com>
2019-08-06 14:48:47 +0200

    Shop updates - one api

--- front-end/src/global-variables.js
+++ front-end/src/global-variables.js
@@ -406,7 +406,7 @@ const globalVariables = {
             ar: 'الكمية'
         },
     LABEL_CURRENCY: {
-            en: 'Pound',
+            en: 'EGP',
             ar: 'جنيه'
         },
     LABEL_PRICE: {
