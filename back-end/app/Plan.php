5ab40a1d73a031a6c4cbcdeb477c0dee2e692308
Ahmed Bally <ahmed30816896@f-eng.tanta.edu.eg>
2019-08-20 16:59:31 +0200

    user plans

---
+++ back-end/app/Plan.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
+
+class Plan extends Model
+{
+    use SoftDelete;
+    protected $fillable = [
+        'name','name_en','description','description_en','commission','price','rules'
+    ];
+    protected $casts = [
+        'deleted_at'=>'datetime',
+        'created_at'=>'datetime',
+        'updated_at'=>'datetime',
+        'rules'=>'json'
+    ];
+    public function Users(){
+        return $this->belongsTo(User::class,'level');
+    }
+
+}
---
+++ back-end/database/migrations/2019_08_20_142728_create_plans_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreatePlansTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('plans', function (Blueprint $table) {
+            $table->increments('id');
+            $table->string('name');
+            $table->string('name_en');
+            $table->text('description');
+            $table->text('description_en');
+            $table->double('commission');
+            $table->double('price');
+            $table->json('rules');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('plans');
+    }
+}
